02ec21f6630b00c2625237a44a86c99d6f089eba
yongun2 <djkye0001@naver.com>
2024-02-11 02:09:14 +0900

    [FEAT] 회원가입, 중복확인 api 정의 및 타입

---
+++ src/api/auth.ts
@@ -0,0 +1,17 @@
+import client from "./client";
+import {
+  APIResponse,
+  CheckDuplicateParam,
+  SignUpRequest,
+  SignUpResponse,
+} from "./types";
+
+const signup = (request: SignUpRequest): Promise<APIResponse<SignUpResponse>> =>
+  client.post("/api/v1/users", request);
+
+const checkDuplicate = (param: keyof CheckDuplicateParam, value: string) =>
+  client.get("/api/v1/users/duplicate", {
+    params: { [param]: value },
+  });
+
+export { checkDuplicate, signup };
